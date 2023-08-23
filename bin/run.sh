7fecfd6b90d09322f07a36e1969dd24e813567d5
shubh <shubhvjain@gmail.com>
2023-08-23 16:08:13 +0200

    fixed the schedule script and path issues with the run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+source ~/.bash_profile
+export PATH="/usr/local/bin:$PATH"
 echo "Running Prediction models at $(date)"
 container_name="codegreen-prediction-tool"
 docker start "$container_name" 
--- bin/schedule.sh
+++ bin/schedule.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
-source .env
 
-# Display initial message
-echo "Hello, this script will allow you to create or modify the schedule for running prediction models for all countries"
+docker_image="codegreen-prediction-tool"
+container_name="codegreen-prediction-tool"
+config_file=".config"
+
+if [ -e "$config_file" ]; then
+    # Load environment variables from the .config file
+    source "$config_file"    
+    echo "Config file loaded"
+else
+    echo "Error: $config_file file does not exist."
+    exit 1
+fi
+
+run_script_full_path=$(pwd)/bin/run.sh
+cron_log_full_path=$(pwd)/cron-run.log
+
+
+
 
 # Check if a cron job is already set up
-existing_cron_job=$(crontab -l | grep "run_models.sh")
+existing_cron_job=$(crontab -l | grep "$run_script_full_path")
 if [ -n "$existing_cron_job" ]; then
     echo "Currently running cron job details:"
     echo "$existing_cron_job"
-    echo "You have an existing cron job. You can modify it (or quit if no changes required) "
 fi
 
-# Prompt user for frequency
-read -p "Enter the frequency of running the script in hours (1-24): " frequency
-
 # Validate the input
-if ! [[ "$frequency" =~ ^[0-9]+$ && "$frequency" -ge 1 && "$frequency" -le 24 ]]; then
-    echo "Invalid input. Please enter a valid number between 1 and 24."
+if ! [[ "$PREDICTIONS_CRON_JOB_FREQ_HOUR" =~ ^[0-9]+$ && "$PREDICTIONS_CRON_JOB_FREQ_HOUR" -ge 1 && "$PREDICTIONS_CRON_JOB_FREQ_HOUR" -le 24 ]]; then
+    echo "Invalid frequency. Please enter a valid number between 1 and 24."
     exit 1
 fi
 
 # Set up or modify the cron job
-cron_command="bash ~/code/bionets/codegreen-prediction-tool/run_models.sh >> $PREDICTIONS_LOG_FOLDER_PATH/cron_job_logs.txt"  # Replace with the actual path to run_model.sh
-existing_cron_job_line="0 */$frequency * * * $cron_command"
+cron_command="bash $run_script_full_path >> $cron_log_full_path"  # Replace with the actual path to run_model.sh
+# existing_cron_job_line="0 */$PREDICTIONS_CRON_JOB_FREQ_HOUR * * * $cron_command"
+existing_cron_job_line="*/$PREDICTIONS_CRON_JOB_FREQ_HOUR * * * * $cron_command"
+
 
 # Remove any existing cron job with the same command
 crontab -l | grep -v "$cron_command" | crontab -
@@ -33,4 +46,5 @@ echo "$existing_cron_job_line" | crontab -
 
 # Get the PID of the newly added cron job
 
-echo "Cron job set up successfully with a frequency of $frequency hours!"
+echo "Cron job set up successfully with a frequency of $PREDICTIONS_CRON_JOB_FREQ_HOUR hours!"
+
