a1df0444fd58cb2f4b92cc9875398b5e4c4428a4
shubh <shubhvjain@gmail.com>
2023-08-23 16:10:12 +0200

    cleanup

--- test.py
+++
@@ -1,8 +0,0 @@
-from dotenv import load_dotenv
-load_dotenv()
-
-import entsoeAPI as e
-
-print(e.entsoe_getDayAheadAggregatedGeneration({"country":"BE","start":"202308151000","end":"202308172100"}))
-print(e.entsoe_getDayAheadGenerationForecastsWindSolar({"country":"BE","start":"202308151000","end":"202308172100"}))
-print(e.getRenewableForecast({"country":"BE","start":"202308151000","end":"202308172100"}))
